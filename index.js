24affd9c8df92010c34d3e2f242aa94417f596ae
Rizwan Baig <therizwanbaig@gmail.com>
2024-10-09 15:50:52 +0500

    chore: update vercel

--- index.js
+++ index.js
@@ -25,10 +25,4 @@ app.use("/api/cars", carRoutes);
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 export default app;
