28fd1b18b6987e8b11a9005b7cd5b9ac453abee3
Rizwan Baig <therizwanbaig@gmail.com>
2024-10-09 15:21:51 +0500

    chore: update app.js

--- app.js
+++ app.js
@@ -23,8 +23,10 @@ app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRoutes);
 app.use("/api/cars", carRoutes);
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Test route for root
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
+
+// Export the app for Vercel
+export default app;
