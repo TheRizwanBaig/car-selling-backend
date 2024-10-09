1aca18fbf01ddf79b9a68340333ea482636e89a6
Rizwan Baig <therizwanbaig@gmail.com>
2024-10-09 15:43:13 +0500

    chore: rename

--- index.js
+++ index.js
@@ -23,8 +23,12 @@ app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRoutes);
 app.use("/api/cars", carRoutes);
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+export default app;
